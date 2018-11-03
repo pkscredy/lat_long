62b5bd8cd216530a891da2f9159d9b78a32ce40b
PrashantSharma <34229777+pkscredy@users.noreply.github.com>
2018-11-03 15:21:51 +0530

    Update constants.py

--- file_upload/address/constants.py
+++ file_upload/address/constants.py
@@ -1,4 +1,4 @@
-API_KEY = 'AIzaSyAkuAaqI7JpMpvnhTy-ujNePPmqBZw9iHw'
+API_KEY = '**********'
 UPLOADED_FILE = '/tmp/prashant/address.csv'
 SUCCESS_MESSAGE = 'Please check /tmp/prashant/ for downloaded file.'
 WARNING_MESSAGE = 'Please Select a File'
